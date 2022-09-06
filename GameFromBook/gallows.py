8a4b1cfa83589fc9e51ffc106b67bd3619081528
Andrey <patron6239@gmail.com>
2022-09-06 22:14:44 +0300

    adding modification

--- GameFromBook/gallows.py
+++ GameFromBook/gallows.py
@@ -1,4 +1,6 @@
 import random
+from typing import List
+
 
 HANGMAN_PICS = ['''
  +---+
@@ -35,15 +37,29 @@ HANGMAN_PICS = ['''
  0   |
 /|\  |
 / \  |
+    ===''', '''
+ +---+
+[O   |
+/|\  |
+/ \  |
+    ===''', '''
+ +---+
+[O]  |
+/|\  |
+/ \  |
     ===''']
 
-words = "аист акула бабуин баран барсук бобр бык верблюд волк воробей ворон выдра голубь гусь жаба зебра змея индюк кит кобра коза козел койот корова кошка кролик крыса курица лама ласка лебедь лев лиса лосось лось лягушка медведь моллюск моль мул муравей мышь норка носорог обезьяна овца окунь олень орел осел панда паук питон попугай пума семга скунс собака сова тигр тритон тюлень утка форель хорек черепаха ястреб ящерица".split(' ')
+words = {'Цвета':'красный оранжевый желтый зеленый синий голубой фиолетовый белый черный коричневый'.split(),
+'Фигуры':'квадрат треугольник прямоугольник круг эллипс ромб трапеция параллелограмм пятиугольник шестиугольник восьмиугольник'.split(),
+'Фрукты':'яблоко апельсин лимон лайм груша мандарин виноград грейпфрут персик банан абрикос манго банан нектарин'.split(),
+'Животные':'аист бабуин баран барсук бык волк зебра кит коза корова кошка кролик крыса лев лиса лось медведь мул мышь норка носорог обезьяна овца олень осел панда пума скунс собака сова тигр тюлень хорек ящерица'.split()}
 
 
-def getRandonWord(wordList):
-    # Эта функция возвращает случайное слово из списка
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+def getRandomWord(wordDict):
+    # Эта функция возвращает случайную строку из переданного словаря списков строк, а так же ключ
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
+    return wordDict[wordKey][wordIndex], wordKey
 
 
 def displayBoard(missedLetters, correctLetters, secretWord):
@@ -89,12 +105,28 @@ def playAgain():
 
 
 print("В И С Е Л И Ц А")
+
+difficulty = ''
+difficulty_list = ["Л", "С", "Т"]
+while difficulty not in difficulty_list:
+    print("Выберите уровень сложности: Л - Легкий, С - Средний, Т - Тяжелый")
+    difficulty = input().upper()
+if difficulty == 'С':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+if difficulty == 'Т':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+
 missedLetters = ''
 correctLetters = ''
-secretWord = getRandonWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 while True:
+    print(f"Секретное слово из набора: {secretSet}")
     displayBoard(missedLetters, correctLetters, secretWord)
 
     # Ввод буквы игроком.
@@ -118,7 +150,7 @@ while True:
     # Проверим, превысил ли игрок лимит попыток.
     if len(missedLetters) == len(HANGMAN_PICS) - 1:
         displayBoard(missedLetters, correctLetters, secretWord)
-        print("Вы исчерпали все попытки! \nНе угаданно букв: " + str(len(missedLetters)) + "угаданно букв: " + str(len(correctLetters)) + ". Было загаданно слово: " + secretWord + ".")
+        print("Вы исчерпали все попытки! \nНе угаданно букв: " + str(len(missedLetters)) + "\nУгаданно букв: " + str(len(correctLetters)) + "\nБыло загаданно слово: " + secretWord + ".")
         gameIsDone = True
 
     # Спросим хочет ли игрок сыграть еще раз.
@@ -127,7 +159,7 @@ while True:
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
-            secretWord = getRandonWord(words)
+            secretWord, secretSet = getRandomWord(words)
         else:
             break
         
